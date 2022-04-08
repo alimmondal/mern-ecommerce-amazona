e2783b4cf3c012e00021b4353786fd8f0dd4ed74
alimmondal <76857187+alimmondal@users.noreply.github.com>
2022-04-08 17:25:51 +0600

    video-3 amazona

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
